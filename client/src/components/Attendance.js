c6c4251204df944300572bef8c9a5257314f565b
A21n <auronmatrix@@users.noreply.github.com>
2018-07-29 13:04:34 +0200

    restify backend + error notifications

--- client/src/components/Attendance.js
+++ client/src/components/Attendance.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
-import { Badge, Card, Button, Row, Col, Avatar, Popover } from 'antd';
-import testStudents from '../data/students.json';
+import { Badge, Button, Avatar, Popover, notification } from 'antd';
 import statusMappings from '../data/statusMappings.json';
 
 class Attendance extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      students: testStudents
-    };
-
+    this.state = {};
     this.setStudentStatus = this.setStudentStatus.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
   }
 
   setStudentStatus(id, status) {
     let updated = this.state.students;
-
-    console.log(id + ' and ' + status);
     updated[id].status = status;
     this.setState({ students: updated });
   }
@@ -34,50 +29,65 @@ class Attendance extends Component {
     }
   }
 
+  componentDidMount() {
+    fetch('/students/G4a')
+      .then(res => res.json())
+      .then(jsonValue => this.setState({ students: jsonValue }))
+      .catch(err => this.setState({ errors: err }));
+  }
+
   render() {
-    const cards = Object.keys(this.state.students).map(id => {
-      const student = this.state.students[id];
+    if (this.state.errors) {
+      notification.error({
+        message: 'Something went wrong',
+        description: this.state.errors.message
+      });
+    }
+    const cards = this.state.students
+      ? Object.keys(this.state.students).map(id => {
+          const student = this.state.students[id];
 
-      const popOverContent = Object.keys(statusMappings).map(statusId => (
-        <Button
-          size="small"
-          style={{ 'border-color': statusMappings[statusId].color }}
-          onClick={() => this.setStudentStatus(id, statusId)}
-        >
-          {statusMappings[statusId].description}
-        </Button>
-      ));
-
-      return (
-        <span style={{ marginRight: 24 }}>
-          <Popover
-            placement="top"
-            title={student.firstname + ' ' + student.lastname}
-            content={popOverContent}
-            trigger="click"
-          >
-            <Badge
-              style={{ backgroundColor: statusMappings[student.status].color }}
-              //className="happy-badge"
-              dot
-            >
-              <Avatar
-                shape="square"
-                size="large"
-                style={{
-                  backgroundColor: 'transparent',
-                  border: '1px solid lightgrey',
-                  color: 'black',
-                  verticalAlign: 'middle'
-                }}
+          return (
+            <span style={{ marginRight: 24 }}>
+              <Popover
+                placement="top"
+                title={student.firstname + ' ' + student.lastname}
+                content={Object.keys(statusMappings).map(statusId => (
+                  <Button
+                    size="small"
+                    style={{ borderColor: statusMappings[statusId].color }}
+                    onClick={() => this.setStudentStatus(id, statusId)}
+                  >
+                    {statusMappings[statusId].description}
+                  </Button>
+                ))}
+                trigger="click"
               >
-                {student.firstname} {student.lastname[0]}
-              </Avatar>
-            </Badge>
-          </Popover>
-        </span>
-      );
-    });
+                <Badge
+                  style={{
+                    backgroundColor: statusMappings[student.status].color
+                  }}
+                  //className="happy-badge"
+                  dot
+                >
+                  <Avatar
+                    shape="square"
+                    size="large"
+                    style={{
+                      backgroundColor: 'transparent',
+                      border: '1px solid lightgrey',
+                      color: 'black',
+                      verticalAlign: 'middle'
+                    }}
+                  >
+                    {student.firstname} {student.lastname[0]}
+                  </Avatar>
+                </Badge>
+              </Popover>
+            </span>
+          );
+        })
+      : '';
 
     return (
       <div>
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const restify = require('restify')
+const fs = require('fs')
+
+var server = restify.createServer()
+server.get('/students/:class', async (req, res, next) => {
+  const content = fs.readFileSync('./data/students.json')
+  res.json(JSON.parse(content))
+  next()
+})
+
+server.listen(8080, () => {
+  console.log('%s listening at %s', server.name, server.url);
+})
