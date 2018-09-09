f9974df766a25e4f853ff3abc7a83c9fc5434f4d
A21n <auronmatrix@@users.noreply.github.com>
2018-09-09 21:51:18 +0200

    Added terrible front-end auth

---
+++ client/src/components/Auth.js
@@ -0,0 +1,150 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom';
+import Attendance from './Attendance';
+import Reports from './Reports';
+import { Button, Input, Form, Icon, Col, notification } from 'antd';
+import sjcl from 'sjcl';
+const FormItem = Form.Item;
+
+// TODO: Replace with actual auth ASAP!
+const terribleTempAuth = {
+  isAuthenticated: false,
+  authenticate(e, cb) {
+    e.preventDefault();
+    const uname = e.target[0].value;
+    const pwd = e.target[1].value;
+    const bitArray = sjcl.hash.sha256.hash(uname + pwd);
+    const hash = sjcl.codec.hex.fromBits(bitArray);
+
+    if (
+      hash ===
+        '4bcd6a1424af95ce0abc806d5934e3571df51a3e8d95611e87a95b1985bc15cd' ||
+      hash ===
+        '866d271ad5e4200cc1102341f1d9e0bbf16f96b83a2cb0d2dd8a932d8f8ac7e0'
+    ) {
+      this.isAuthenticated = true;
+      setTimeout(cb, 100);
+    } else {
+      notification.error({
+        message: 'Invalid Username/Password Specified'
+      });
+    }
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+const Public = () => <h3>Public</h3>;
+const Protected = () => <h3>Protected</h3>;
+
+class Login extends React.Component {
+  state = {
+    redirectToReferrer: false
+  };
+  login = () => {
+    terribleTempAuth.authenticate(() => {
+      this.setState(() => ({
+        redirectToReferrer: true
+      }));
+    });
+  };
+  render() {
+    const { from } = this.props.location.state || { from: { pathname: '/' } };
+    const { redirectToReferrer } = this.state;
+
+    if (redirectToReferrer === true) {
+      return <Redirect to={from} />;
+    }
+
+    return (
+      <div>
+        <p>You must log in to view the page</p>
+        <button onClick={this.login}>Log in</button>
+      </div>
+    );
+  }
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      terribleTempAuth.isAuthenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/login',
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+
+const AuthComponent = withRouter(
+  ({ history }) =>
+    terribleTempAuth.isAuthenticated ? (
+      <p>
+        <Button
+          style={{ position: 'absolute', top: '10px', right: '10px' }}
+          icon="logout"
+          onClick={() => {
+            terribleTempAuth.signout(() => history.push('/'));
+          }}
+        />
+      </p>
+    ) : (
+      <Col span={12}>
+        <h2>Login</h2>
+        <Form
+          onSubmit={e => {
+            e.preventDefault();
+            terribleTempAuth.authenticate(e, () => history.push('/attendance'));
+          }}
+          className="login-form"
+        >
+          <FormItem>
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />
+          </FormItem>
+          <FormItem>
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />
+          </FormItem>
+          <Button
+            icon="login"
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Login
+          </Button>
+        </Form>
+      </Col>
+    )
+);
+
+export default function Auth() {
+  return (
+    <div>
+      <AuthComponent />
+      <PrivateRoute path="/attendance" exact="false" component={Attendance} />
+      <PrivateRoute path="/reports" component={Reports} />
+    </div>
+  );
+}
--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -2,8 +2,7 @@ import { Layout, Menu } from 'antd';
 import React from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-import Attendance from './Attendance';
-import Reports from './Reports';
+import Auth from './Auth';
 
 const { Content, Sider } = Layout;
 
@@ -14,13 +13,13 @@ export default props => (
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={['2']}
           style={{ height: '100%', borderRight: 0 }}
         >
-          <Menu.Item key="1">
-            <NavLink to="/">Attendance</NavLink>
-          </Menu.Item>
           <Menu.Item key="2">
+            <NavLink to="/attendance">Attendance</NavLink>
+          </Menu.Item>
+          <Menu.Item key="3">
             <NavLink to="/reports">Reports</NavLink>
           </Menu.Item>
         </Menu>
@@ -28,8 +27,7 @@ export default props => (
       <Layout>
         <Content style={{ background: '#fbfbfb' }}>
           <div style={{ padding: '25px 5px 5px 5px', background: '#fbfbfb' }}>
-            <Route key="1" path="/" exact="false" component={Attendance} />
-            <Route key="2" path="/reports" component={Reports} />
+            <Auth />
           </div>
         </Content>
       </Layout>
